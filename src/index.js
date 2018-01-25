16c105c8d2eda27b8a0119894df2cf75fe1cb713
KCD <Kayssidi@users.noreply.github.com>
2018-01-25 17:33:53 +0100

    Use grommet and grommet-css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
+
+import '../node_modules/grommet-css'
+// or https://rawgit.com/grommet/grommet/stable/grommet.min.css ?
+
 import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
+
+import Button from 'grommet/components/Button';
+import Toast from 'grommet/components/Toast';
 
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
 };
 
+const bg = {
+
+  background: 'neutral-1',
+};
 const App = () => (
   <div style={styles}>
     <Hello name="CodeSandbox" />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
+    <Button primary label='Label'
+      hoverIndicator={bg} 
+     />
+     <Toast>Hello</Toast>
   </div>
 );
 
