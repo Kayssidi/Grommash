f62bf6dc0c01c4027ac5c043e64e0c032d2eafd2
KCD <Kayssidi@users.noreply.github.com>
2018-02-02 18:13:50 +0100

    improve reservationCard

--- src/components/reservationCard.js
+++ src/components/reservationCard.js
@@ -4,11 +4,10 @@ import PropTypes from "prop-types";
 import Card from 'grommet/components/Card';
 import Form from 'grommet/components/Form';
 import FormField from 'grommet/components/FormField';
-import DateTime from 'grommet/components/DateTime';
+import Select from 'grommet/components/Select';
 
 import Footer from 'grommet/components/Footer';
 import Button from 'grommet/components/Button';
-import RadioButton from 'grommet/components/RadioButton';
 import Paragraph from 'grommet/components/Paragraph';
 
 // http://react-day-picker.js.org/docs/getting-started/
@@ -45,7 +44,13 @@ const WEEKDAYS_SHORT = ['Di', 'Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa'];
 class ReservationCard extends React.Component {
   state = {
     stateDateSelected:undefined,
+    stateTimeSelected:undefined,
   };
+
+  buttonTxt = `Réserver le ${moment(this.state.stateDateSelected).format('DD/MM/YYYY')} 
+              à ${moment('09h00', ['H:m']).format('HH:mm')}`;
+              //à ${moment(this.state.stateTimeSelected, ['h:m a', 'H:m']).format('HH:mm')}`;
+
 
   constructor(props) {
     super(props);
@@ -53,6 +58,7 @@ class ReservationCard extends React.Component {
 
     // This binding is necessary to make `this` work in the callback
     this.handleDayClick = this.handleDayClick.bind(this);
+    this.handleTimeClick = this.handleTimeClick.bind(this);
   }
 
   handleDayClick(day)
@@ -67,6 +73,12 @@ class ReservationCard extends React.Component {
     {
       this.setState({ stateDateSelected: day });
     }
+  }
+
+  handleTimeClick(time)
+  {
+    console.log(time);
+    this.setState({ stateTimeSelected : time.value});
   }
 
   render() {
@@ -93,19 +105,29 @@ class ReservationCard extends React.Component {
               this.state.stateDateSelected ?
               <div>
                   <Paragraph>Horaires disponibles le {moment(this.state.stateDateSelected).format('DD/MM/YYYY')}:</Paragraph>
-                <RadioButton id="r1" label="09h00"/>
-                <RadioButton id="r2"label="10h00" />
+                  <Select placeHolder={this.state.stateTimeSelected}
+                    options={['09:00', '10h00', '11h00', '12h00', '13h00', '14h00', '15h00', '16h00']}
+                    value={this.state.stateTimeSelected}
+                    onChange={ this.handleTimeClick}
+                  />
               </div>:
               <Paragraph>Choisir le jour de la séance</Paragraph>
             }
 
           </FormField>
         
+          
+
           <Footer pad={{ "vertical": "medium" }}>
-              <Button label='Continuer'
+          {
+            (this.state.stateDateSelected && this.state.stateTimeSelected) ?
+                <Button label={this.buttonTxt}
                       type='submit'
-                      primary={true} />
+                      primary={true} />:
+              <div/>
+          }
           </Footer>
+
         </Form>
       </Card>
     );
